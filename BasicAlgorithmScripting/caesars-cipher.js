d23b31c81f0cbbc040078cdb08e79d45cf226f10
Rob <robjrenwick@gmail.com>
2016-02-03 22:03:24 +1000

    completed most of caesars cipher
    need to test and do final clean up

--- BasicAlgorithmScripting/caesars-cipher.js
+++ BasicAlgorithmScripting/caesars-cipher.js
@@ -2,54 +2,45 @@ function rot13(str) { // LBH QVQ VG!
 // A = 65, M = 77, Z = 90
 
 var newArr = [];
+var cTest = "";
+var cNum = 0;
 // split string into array
 var arr = str.split('');
 
 console.log(arr);
 
 // loop through each element in array
-
+for (var i=0;i < arr.length; i++) {
+  cTest = arr[i].toUpperCase();
+  console.log(cTest);
     // test if uppercase alphabetical
+    cNum = cTest.charCodeAt(0);
+    if (cNum >= 65 && cNum <=90) {
+      // test if first or 2nd half of alphabet A-M or N-Z
+      if (cNum >=78) {
+        cNum -= 13;
+      } else {
+        cNum += 13;
+      }
+      str = String.fromCharCode(cNum);
+    } else {
+      str = cTest;
+    }
+    newArr.push(str);
+    console.log(newArr);
 
-        // test if first or 2nd half of alphabet A-M or N-Z
+
             // convert string to charCode
             // add or minus 13 to/from charCode
             // convert to string fromCharCode
 
     // push string to new array
-        newArr = newArr.push(testChar);
+        //newArr = newArr.push(testChar);
 
 // end loop
-
-
-// BELOW IS TEST CODE
- var test = "b";
- console.log(test);
- test = test.toUpperCase();
- console.log(test);
- var t2;
- t2 = test.charCodeAt(0);
- // check if uppercase alphabetical characters
- if (t2>=65 && t2 <=90) {
-
-   if (t2 >= 78) {
-     var t3 = t2 - 13;
-     console.log(t3);
-   }
-   else
-   {
-    console.log(t2);
-    var t3 = t2 + 13;
-    console.log(t3);
 }
-  var t4 = String.fromCharCode(t3);
-    console.log(t4);
-    str = t4;
-  }
-  else
-  {
-    str = test;
-  }
+str = newArr.join('');
+
 
   console.log('str is - ' + str);
   return str;
