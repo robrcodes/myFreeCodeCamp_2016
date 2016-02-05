635fc811ce01767aacdf914fa376ce1068eb5396
Rob <robjrenwick@gmail.com>
2016-02-05 16:18:57 +1000

    completed caesars cipher
    One of the simplest and most widely known ciphers is a Caesar cipher,
    also known as a shift cipher. In a shift cipher the meanings of the
    letters are shifted by some set amount.

--- BasicAlgorithmScripting/caesars-cipher.js
+++ BasicAlgorithmScripting/caesars-cipher.js
@@ -1,50 +1,31 @@
-function rot13(str) { // LBH QVQ VG!
-// A = 65, M = 77, Z = 90
-
-var newArr = [];
-var cTest = "";
-var cNum = 0;
-// split string into array
-var arr = str.split('');
-
-console.log(arr);
-
-// loop through each element in array
-for (var i=0;i < arr.length; i++) {
-  cTest = arr[i].toUpperCase();
-  console.log(cTest);
-    // test if uppercase alphabetical
-    cNum = cTest.charCodeAt(0);
-    if (cNum >= 65 && cNum <=90) {
-      // test if first or 2nd half of alphabet A-M or N-Z
-      if (cNum >=78) {
-        cNum -= 13;
+function rot13(str) {
+  var newArr = [];
+  var cTest = "";
+  var cNum = 0;
+  // split string into array
+  var arr = str.split('');
+  // loop through each element in array
+  for (var i=0;i < arr.length; i++) {
+    cTest = arr[i].toUpperCase();
+      cNum = cTest.charCodeAt(0);
+      // test if uppercase alphabetical
+      if (cNum >= 65 && cNum <=90) {
+        // test if A-M or N-Z
+        if (cNum >=78) {
+          cNum -= 13;
+        } else {
+          cNum += 13;
+        }
+        str = String.fromCharCode(cNum);
       } else {
-        cNum += 13;
+        str = cTest;
       }
-      str = String.fromCharCode(cNum);
-    } else {
-      str = cTest;
-    }
-    newArr.push(str);
-    console.log(newArr);
-
-
-            // convert string to charCode
-            // add or minus 13 to/from charCode
-            // convert to string fromCharCode
-
-    // push string to new array
-        //newArr = newArr.push(testChar);
-
-// end loop
-}
-str = newArr.join('');
-
-
-  console.log('str is - ' + str);
+      newArr.push(str);
+  // end loop
+  }
+  str = newArr.join('');
+  console.log(str);
   return str;
 }
-
 // Change the inputs below to test
 rot13("SERR PBQR PNZC");
