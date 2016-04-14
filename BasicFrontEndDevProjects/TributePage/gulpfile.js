49d9f4f617d1b908e96f76ca3b4340a74318a000
Rob <robjrenwick@gmail.com>
2016-04-15 08:30:01 +1000

    added readme and clean up gulpfile

--- BasicFrontEndDevProjects/TributePage/gulpfile.js
+++ BasicFrontEndDevProjects/TributePage/gulpfile.js
@@ -1,84 +1,17 @@
+// Simple gulp setup with only 1 task:
+// watch for changes and reload browser!
+
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
-var sourcemaps = require('gulp-sourcemaps');
-var concat = require('gulp-concat');
-var uglifycss = require('gulp-uglifycss');
-var notify = require('gulp-notify');
-var autoprefixer = require('gulp-autoprefixer');
-var pixrem = require('gulp-pixrem');
-var plumber = require('gulp-plumber');
-var picshrink = require('gulp-imagemin');
-
-var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'expanded'
-};
-
-var autoprefixerOptions = {
-  browsers: ['last 2 versions', '> 5%', 'Firefox ESR'],
-  cascade: false
-};
-
-// object containing file paths
-var paths = {
-  css:['./src/css/main.css']
-  ,cssdist:['./dist/css/']
-  ,cssin:['./dist/css/allcss.css']
-  ,cssminout:['./dist/css/*.min.css']
-  ,sass:['./src/scss/main.scss']
-  ,sasspartials:['./src/scss/partials/*.scss']
-};
 
 // default task
 gulp.task('default', ['serve']);
-// gulp.task('default', ['serve', 'doSass', 'allcss']);
-// concatinate css to dist folder
-// this version not minified
-gulp.task('allcss', function() {
-  console.log('Gulp is running allcss task...');
-  return gulp.src(paths.css)
-  .pipe(concat('allcss.css'))
-  .pipe(gulp.dest('./dist/css/'));
-});
 
-gulp.task('mincss', function() {
-  return gulp.src(paths.css)
-    .pipe(concat('allcss.min.css'))
-    .pipe(uglifycss({
-      "max-line-len": 80 }))
-    .pipe(gulp.dest('./dist/css/'));
-});
-
-// Static Server + watching scss/html files
+// Static Server + watching html files
 gulp.task('serve', function() {
     browserSync.init({
         server: "./"
     });
         // reload browser
     gulp.watch("./*.html").on('change', browserSync.reload);
-});
-
-gulp.task('doSass', function() {
-  console.log('Gulp is running doSass task...');
-  return gulp.src(paths.sass)
-      .pipe(plumber())
-      .pipe(sourcemaps.init())
-      .pipe(sass(sassOptions).on('error', notify.onError(function (error) {
-return "Problem file : " + error.message;
-})))
-      .pipe(autoprefixer(autoprefixerOptions))
-      .pipe(sourcemaps.write('./maps'))
-      .pipe(pixrem({ rootValue: '10px' }))
-      .pipe(plumber.stop())
-      .pipe(gulp.dest('./src/css/'))
-      .pipe(browserSync.stream());
-      console.log('Gulp is finished doSass task...');
-});
-
-//optimise images
-gulp.task('image-opt', function() {
-  return gulp.src('./src/images/**/*')
-    .pipe(picshrink())
-    .pipe(gulp.dest('./dist/images/'));
 });
